9ede50e0793b1e61036f08148c832d23e91b1833
Nicolas OGIER <nicolas.ogier@gmail.com>
2024-11-10 17:54:51 +0100

    🚀feat(correction) ajout de QR codes pour les exercices 🛠️refactor(correction) amélioration de la gestion des fichiers PDF ⚙️chore(correction) ajout de l'URL de la page GitHub

--- correction.py
+++ correction.py
@@ -6,6 +6,7 @@ import subprocess
 input_file = 'test.tex'
 # Chemin vers le dépôt Git
 github_repo_dir = '/Users/nicolasogier/workspaces/Latex/GithubPage'
+github_page_url = 'https://opware2000.github.io/TestGithubPageLatex/'
 
 # Fonction pour extraire les corrections
 
@@ -71,6 +72,23 @@ def copy_files(corrections):
             # Copie le fichier PDF vers le dossier GitHub Pages
             shutil.copy(pdf_filename, github_repo_dir)
 
+
+def insert_qr_codes(input_file, corrections):
+    with open(input_file, 'r') as file:
+        content = file.readlines()
+
+    with open(input_file, 'w') as file:
+        for line in content:
+            file.write(line)
+            if r'\begin{Exercice}' in line:
+                for filename, _ in corrections:
+                    pdf_filename = filename.replace('.tex', '.pdf')
+                    base_filename = os.path.basename(pdf_filename)
+                    # Ajouter le QR code pour chaque PDF
+                    file.write(r'\begin{center}' + '\n')
+                    file.write(
+                        r'\qrcode{' + github_page_url + base_filename + '}' + '\n')
+                    file.write(r'\end{center}' + '\n')
 # Main
 if __name__ == '__main__':
     corrections = extract_corrections(input_file)
@@ -79,6 +97,10 @@ if __name__ == '__main__':
     # Copier les fichiers PDF vers le répertoire GitHub Pages
     copy_files(corrections)
 
+    # Insérer les QR codes dans le fichier d'énoncé
+    insert_qr_codes(input_file, corrections)
+
+    # Compiler le fichier d'énoncé après l'insertion des QR codes
     compile_statement(input_file)
 
     # Nettoyer les fichiers temporaires
